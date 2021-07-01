ab306f318195eb3d47bf7bb935bdc1f60548527a
Andrew Stewart <a.stewart@strath.ac.uk>
2021-07-01 14:42:39 +0100

    Updated handling of edge cases for vectorisation of words

--- Chapter 3/IMDb_exercise.py
+++ Chapter 3/IMDb_exercise.py
@@ -8,28 +8,50 @@ import os
 import re
 
 def VectoriseStringData(str_dir, vec_dir, dict_in):
-    #for file in os.listdir(str_dir):
-    string = open(str_dir + "\\" + "18_7.txt", encoding = 'utf-8', mode = 'r')
-    split_string = re.split("[\s*`,.<>\/!?()]", string.read())
-    joined_string = " ".join([i.lower() for i in split_string if i not in ["", "br"]])
-    print(joined_string)
-    vectorised_string = [dict_in[stringval] for stringval in joined_string.split(" ")]
-    print(vectorised_string)
-    string.close()
+    count = 0
+    for file in os.listdir(str_dir):
+        string = open(str_dir + "\\" + file, encoding = 'utf-8', mode = 'r')
+        split_string = re.split('[\s*`,":;.<>\/!?()]', string.read())
 
-'''
-    ratings = pd.read_csv('../.data/IMDB/aclImdb/imdbEr.txt', sep = " ", header = None, dtype = np.float32)
-    ratings.columns = ["word_rating"]
+        for idx, value in enumerate(split_string):
+            if value.endswith("'"):
+                rem = -1
+            elif value.endswith("'s"):
+                rem = -2
+            else:
+                rem = len(value)
+            split_string[idx] = value[:rem]
 
-    vocab = pd.read_csv('../.data/IMDB/aclImdb/imdb.vocab', sep = " ", header = None, dtype = np.str)
-    vocab.columns = ["word"]
+        # split_string = [value[:-1] for value in split_string if value.endswith("'")]
+        joined_string = " ".join([i.lower() for i in split_string if i not in ["", "br"]])
+        print(joined_string)
 
-    voc_scores = pd.concat([vocab, ratings], axis = 1, join = 'inner')
-    print(voc_scores.head())
+        '''
+            find a way to convert numbers to text-based numbering (e.g. 35 to thirty-five) maybe check Stackoverflow for solution
+        '''
 
-    voc_scores = voc_scores[:30000]
-    voc_scores.to_csv('../.data/IMDB/Custom/vocab_scores.csv')
-'''
+        vectorised_string = [dict_in[stringval] for stringval in joined_string.split(" ")]
+        print(vectorised_string)
+        string.close()
+
+        if count < 20:
+            count += 1
+        else:
+            break
+
+# IMDB_dataset = torchtx.datasets.IMDB(root = '../.data/')
+
+ratings = pd.read_csv('../.data/IMDB/aclImdb/imdbEr.txt', sep = " ", header = None, dtype = np.float32)
+ratings.columns = ["word_rating"]
+
+vocab = pd.read_csv('../.data/IMDB/aclImdb/imdb.vocab', sep = " ", header = None, dtype = np.str)
+vocab.columns = ["word"]
+
+voc_scores = pd.concat([vocab, ratings], axis = 1, join = 'inner')
+print(voc_scores.head())
+
+voc_scores = voc_scores[:30000]
+voc_scores.to_csv('../.data/IMDB/Custom/vocab_scores.csv')
 
 vocab_scores = pd.read_csv('../.data/IMDB/Custom/vocab_scores.csv')
 vocab_ratings = dict([(row["word"], row["word_rating"]) for label, row in vocab_scores.iterrows()])
